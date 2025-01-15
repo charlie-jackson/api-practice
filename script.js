25a2b718cd58077915182ae0f08daf3f76b2bc5d
Charlie Jackson <cgj@cgjmedia.uk>
2025-01-15 16:32:40 +0000

    Undo delete files

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // function fetchWeatherData(city) {
-//     fetch(`http://api.weatherstack.com/current?access_key=a104815df953f673fb0b78342c9d3793&query=London`)
+//     fetch
 //         .then(resp => resp.json())
 //         .then(data => addWeather(data))
 //         .catch(err => console.log(err))
