ed1310d746713584483b6e741ac3553df22f7b4e
Charlie Jackson <cgj@cgjmedia.uk>
2025-01-15 14:08:54 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// function fetchWeatherData(city) {
+//     fetch(`http://api.weatherstack.com/current?access_key=a104815df953f673fb0b78342c9d3793&query=London`)
+//         .then(resp => resp.json())
+//         .then(data => addWeather(data))
+//         .catch(err => console.log(err))
+// }
+
+// console.log(fetchWeatherData());
+
